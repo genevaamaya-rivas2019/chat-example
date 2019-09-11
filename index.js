397b6c724bbf1279d7b920a0745f7f26e77ebb23
2ndyrGroupC <2ndyrGroupC@pn.local>
2019-09-11 16:08:21 +0800

    Added functions

--- index.js
+++ index.js
@@ -4,43 +4,43 @@ var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function (socket) {
-  socket.on('chat message', function (msg) {
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
     io.emit('chat message', msg);
     console.log(msg)
   });
 });
 
-http.listen(port, function () {
+http.listen(port, function(){
   console.log('listening on *:' + port);
 });
 
 
 //listen on every connection
 io.on('connection', (socket) => {
-  console.log('New user connected')
+	console.log('New user connected')
 
-  //default username
+	//default username
   socket.username = "Anonymous"
+  
+    //listen on change_username
+    socket.on('change_username', (data) => {
+        socket.username = data.username
+        console.log(socket.username)
+    })
 
-  //listen on change_username
-  socket.on('change_username', (data) => {
-    socket.username = data.username
-    console.log(socket.username)
-  })
+    //listen on new_message
+    socket.on('new_message', (data) => {
+        //broadcast the new message
+        io.sockets.emit('new_message', {message : data.message, username : socket.username});
+    })
 
-  //listen on new_message
-  socket.on('new_message', (data) => {
-    //broadcast the new message
-    io.sockets.emit('new_message', { message: data.message, username: socket.username });
-  })
-
-  //listen on typing
-  socket.on('typing', (data) => {
-    socket.broadcast.emit('typing', { username: socket.username })
-  })
+    //listen on typing
+    socket.on('typing', (data) => {
+    	socket.broadcast.emit('typing', {username : socket.username})
+    })
 })
