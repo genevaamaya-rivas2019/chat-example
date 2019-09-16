a43638963fd77e8a6087d56fd85ed58b52d11d95
genevaamaya-rivas2019 <geneva.rivas@student.passerellesnumeriques.org>
2019-09-16 14:22:09 +0800

    Modified the function.

--- index.js
+++ index.js
@@ -4,43 +4,65 @@ var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
+io.on('connection', function (socket) {
+  socket.on('chat message', function (msg) {
     io.emit('chat message', msg);
     console.log(msg)
   });
 });
 
-http.listen(port, function(){
+http.listen(port, function () {
   console.log('listening on *:' + port);
 });
 
 
+//var numUsers = 0;
+
 //listen on every connection
 io.on('connection', (socket) => {
-	console.log('New user connected')
-
-	//default username
+  console.log('New user connected')
+  //var addedUser = false;
+  //default username
   socket.username = "Anonymous"
-  
-    //listen on change_username
-    socket.on('change_username', (data) => {
-        socket.username = data.username
-        console.log(socket.username)
-    })
 
-    //listen on new_message
-    socket.on('new_message', (data) => {
-        //broadcast the new message
-        io.sockets.emit('new_message', {message : data.message, username : socket.username});
-    })
+  //listen on change_username
+  socket.on('change_username', (data) => {
+    socket.username = data.username
+    console.log(socket.username)
+  })
 
-    //listen on typing
-    socket.on('typing', (data) => {
-    	socket.broadcast.emit('typing', {username : socket.username})
-    })
+  //listen on new_message
+  socket.on('new_message', (data) => {
+    //broadcast the new message
+    io.sockets.emit('new_message', { message: data.message, username: socket.username });
+  })
+
+  //listen on typing
+  socket.on('typing', (data) => {
+    socket.broadcast.emit('typing', { username: socket.username })
+  })
+
+  // socket.on('add user', (username) => {
+  //   if (addedUser) return;
+
+  //   // we store the username in the socket session for this client
+  //   socket.username = username;
+  //   ++numUsers;
+  //   addedUser = true;
+  //   socket.emit('login', {
+  //     numUsers: numUsers
+  //   })
+
+  // echo globally (all clients) that a person has connected
+  //   socket.broadcast.emit('user joined', {
+  //     username: socket.username,
+  //     numUsers: numUsers
+  //   });
+  //   console.log(numUsers);
+  // })
+
 })
